8fec5ae4b960b29b036f28a14fed2af2933b7146
Juliusz Piskor <juliuszpiskor@gmail.com>
2023-01-26 21:16:49 +0100

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      MultiFib (Zadanie 2)
+      <br></br>
+      Juliusz Piskor IMST 2.3
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
